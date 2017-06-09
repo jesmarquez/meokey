559e2e18a61cdbcfa5dc8b8e0aa55d095ec1957c
Jesus Marquez <jesmqz@gmail.com>
2017-06-08 23:12:46 -0500

    Crea head

--- src/cliente/contenedor/Layout.js
+++ src/cliente/contenedor/Layout.js
@@ -1,7 +1,12 @@
 import AppBarMain from './AppBarMain'
+import Head from 'next/head'
 
 const Layout = (props) => (
   <div>
+    <Head>
+      <title>MeoKey</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <AppBarMain />
     {props.children}
   </div>  
