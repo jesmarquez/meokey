bc969014b2d14e8db3a3586ecad2636497222ade
jesmarquez <jesmqz@gmail.com>
2017-06-13 17:22:43 -0500

    Crea searchbox

--- pages/visor.js
+++ pages/visor.js
@@ -1,10 +1,12 @@
 import Layout from '../src/cliente/contenedor/Layout'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Lista from '../src/cliente/contenedor/Lista'
+import SearchBox from '../src/cliente/contenedor/SearchBox'
 
 const Visor = () => (
   <MuiThemeProvider>
     <Layout>
+      <SearchBox />
       <Lista />
     </Layout>
   </MuiThemeProvider>
---
+++ src/cliente/contenedor/SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TextField from 'material-ui/TextField'
+
+const SearchBox = () => (
+  <div className="container-search">
+    <TextField
+      hintText="Ingrese el servidor"
+    />
+  </div>
+)
+
+export default SearchBox
